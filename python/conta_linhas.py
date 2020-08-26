0288f37ecb02fadd2eb1ac8fdaf7894784bfec65
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2020-08-26 18:01:48 -0300

    Documentando arquivo 'conta_linhas'

--- python/conta_linhas.py
+++ python/conta_linhas.py
@@ -1,8 +1,16 @@
+#importação do módulo sys
 import sys
+
+#Caso um segundo parâmetro, contendo o caminho de um arquivo,
+#não seja informado o programa retorna apenas o conteúdo do print().
 
 if len(sys.argv) == 1:
     print("Indique o arquivo a ser processado como parâmetro. Ex: " + sys.argv[0] + " exemplo.txt")
     exit(1)
+
+#Abre o arquivo passado no segundo argumento e em seguida
+#conta quantas quebras de linha o texto possui, informando
+#ao usuário o número de linhas que o arquivo possui
 
 with open(sys.argv[1]) as open_f:
     print(open_f.read().count('\n') + 1)
