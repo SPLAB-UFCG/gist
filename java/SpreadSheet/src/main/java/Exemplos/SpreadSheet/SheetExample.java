c798419181bf6134ca9a900d85a2944d2cb7153f
Gabriel Carvalho <gabriel.fonseca@ccc.ufcg.edu.br>
2020-08-29 15:06:55 -0300

    Criado um exemplo de programa que consome informação de uma tabela exemplo que simula notas de alunos em uma materia. É feita a soma dos valores e a media deles, printando no console quem foi aprovado ou reprovado

---
+++ java/SpreadSheet/src/main/java/Exemplos/SpreadSheet/SheetExample.java
@@ -0,0 +1,113 @@
+package Exemplos.SpreadSheet;
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START sheets_quickstart]
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class SheetExample {
+    private static final String APPLICATION_NAME = "Sheet API example";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+
+
+    /**
+     * Olha a nota dos alunos na planilha, calcula a média e indica quem foi aprovado
+     * planilha utilizada:
+     * https://docs.google.com/spreadsheets/d/1RBo9Se3c19e1s7r_IbL97Gb3HoMbum1bzo56psYafCw/edit#gid=0
+     */
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        final String spreadsheetId = "1RBo9Se3c19e1s7r_IbL97Gb3HoMbum1bzo56psYafCw";
+        final String rangeRead = "Página1!A:E";
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, rangeRead)
+                .execute();
+        // Salvo o valor das células da tabela pelo conjunto de linhas.
+        // Verifica-se se não está vazia
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            for (List row : values) {
+                // Print columns A and E, which correspond to indices 0 and 4.
+            	if(aprovado((String) row.get(1), (String) row.get(2), (String) row.get(3))) {
+            		System.out.println( row.get(0) + ": aprovado");
+            	} else {
+            		System.out.println(row.get(0) + ": reprovado");
+            	}
+            }
+        }
+    }
+    
+    private static boolean aprovado(String nota1,String nota2, String nota3) {
+    	int n1 = Integer.parseInt(nota1);
+    	int n2 = Integer.parseInt(nota2);
+    	int n3 = Integer.parseInt(nota3);
+    	if((n1 + n2 + n3)/3 >= 7) {
+    		return true;
+    	}
+    	return false;
+    }
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = SheetExample.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+}
